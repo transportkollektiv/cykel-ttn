d73008d3a03eb0d1ec3dbdbe3d443764c7ea06ac
Maximilian Richt <maxi@richt.name>
2019-08-21 22:39:33 +0200

    add prometheus metrics for battery voltage

--- app.py
+++ app.py
@@ -5,9 +5,14 @@ import atexit
 import requests
 import os
 
+from prometheus_client import Gauge, start_http_server
+
 app_id = os.environ['TTN_APP_ID']
 access_key = os.environ['TTN_ACCESS_KEY']
 endpoint = os.environ['ENDPOINT']
+port = int(os.getenv('PORT', 8080))
+
+gauge = Gauge('bike_battery_volts', 'bike battery voltage', ['bike_number'])
 
 def uplink_callback(msg, client):
 	try:
@@ -23,8 +28,10 @@ def uplink_callback(msg, client):
 		}
 		resp = requests.post(endpoint, data=update)
 		print(resp)
+		gauge.labels(bike_number=bike_number).set(data.vbat)
 	except e:
 		print(e)
+
 
 handler = ttn.HandlerClient(app_id, access_key)
 mqtt_client = handler.data()
@@ -36,6 +43,8 @@ atexit.register(close_mqtt)
 
 mqtt_client.set_uplink_callback(uplink_callback)
 mqtt_client.connect()
+
+start_http_server(port)
 
 try:
 	while 1:
